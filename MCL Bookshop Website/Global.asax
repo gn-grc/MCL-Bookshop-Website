6fdc1f60acb13a142e614fbfbaece5affa08ea03
gn-grc <genejogarcia.gg@gmail.com>
2020-03-19 15:05:27 +0800

    init

---
+++ MCL Bookshop Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MCL_Bookshop_Website.MvcApplication" Language="C#" %>
---
+++ MCL Bookshop Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MCL_Bookshop_Website.Startup))]
+namespace MCL_Bookshop_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
